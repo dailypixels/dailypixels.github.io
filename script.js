e87332892c331e981e00208ec59922b0082b011a
daily pixels <dailypixelsite@gmail.com>
2026-01-30 22:01:45 +0500

    update

--- script.js
+++ script.js
@@ -1310,26 +1310,3 @@ setTimeout(() => {
   document.body.classList.add('show-sections');
 }, 600); // adjust delay to match hero animation end
 
-// Add this script
-const texts = ["Web Development", "UI/UX Design", "Digital Solutions"];
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-
-(function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
-    
-    document.querySelector(".typing-text").textContent = letter;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-        setTimeout(type, 2000);
-    } else {
-        setTimeout(type, 100);
-    }
-}());
